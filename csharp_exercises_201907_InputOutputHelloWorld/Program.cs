fa92c3f5b54106a0a5ee765d92701e8eab122a9b
msunkist99 <msunkist99@gmail.com>
2019-12-06 16:28:11 -0600

    Add project files.

---
+++ csharp_exercises_201907_InputOutputHelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace csharp_exercises_201907_InputOutputHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name - ");
+
+            string input;
+            input = Console.ReadLine();
+            Console.WriteLine("Hello " + input);
+
+            /*
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello " + input);
+            */
+
+            //Console.WriteLine("Hello " + Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+}
